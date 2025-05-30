088001b8c4a3b3aad774aec6836833ffcfcb2fb6
Lqnk4 <internalbeast109@gmail.com>
2025-05-29 21:58:07 -0700

    Command Buffers

--- app/Frame.hs
+++ app/Frame.hs
@@ -1,19 +1,20 @@
 module Frame where
 
+import Control.Monad.Trans.Resource
+import Data.Foldable
 import Data.Vector (Vector)
+import qualified Data.Vector as V
 import Data.Word
 import qualified Graphics.UI.GLFW as GLFW
+import Init
 import RefCounted
 import Swapchain
 import Vulkan.Core10
-import qualified Vulkan.Core10.FundamentalTypes as Extent2D (Extent2D(..))
+import qualified Vulkan.Core10.CommandPool as CommandPoolCreateInfo (CommandPoolCreateInfo (..))
+import qualified Vulkan.Core10.FundamentalTypes as Extent2D (Extent2D (..))
 import qualified Vulkan.Core10.Pass as FramebufferCreateInfo (FramebufferCreateInfo (..))
 import Vulkan.Extensions.VK_KHR_surface
 import Vulkan.Zero
-import qualified Data.Vector as V
-import Init
-import Control.Monad.Trans.Resource
-import Data.Foldable
 
 numConcurrentFrames :: Int
 numConcurrentFrames = 3
@@ -27,7 +28,23 @@ data Frame = Frame
     , fRenderPass :: RenderPass
     , fFramebuffers :: Vector Framebuffer
     , fReleaseFramebuffers :: RefCounted
+    , fRecycledResources :: RecycledResources
     }
+
+data RecycledResources = RecycledResources
+    { fImageAvailableSemaphore :: Semaphore
+    , fRenderFinishedSemaphore :: Semaphore
+    , fCommandPool :: CommandPool
+    }
+
+createCommandPool :: (MonadResource m) => DeviceParams -> m (ReleaseKey, CommandPool)
+createCommandPool devParams = do
+    let poolInfo =
+            zero
+                { CommandPoolCreateInfo.flags = COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+                , CommandPoolCreateInfo.queueFamilyIndex = dpGraphicsQueueFamilyIndex devParams
+                }
+    withCommandPool (dpDevice devParams) poolInfo Nothing allocate
 
 createFramebuffers :: (MonadResource m) => DeviceParams -> RenderPass -> SwapchainResources -> m (RefCounted, Vector Framebuffer)
 createFramebuffers devParams renderPass SwapchainResources{..} = do
@@ -40,8 +57,7 @@ createFramebuffers devParams renderPass SwapchainResources{..} = do
                 , FramebufferCreateInfo.height = Extent2D.height siImageExtent
                 , layers = 1
                 }
-    (framebufferKeys, frameBuffers) <- fmap V.unzip . V.forM srImageViews $ \imageView -> 
+    (framebufferKeys, frameBuffers) <- fmap V.unzip . V.forM srImageViews $ \imageView ->
         withFramebuffer (dpDevice devParams) (framebufferInfo imageView) Nothing allocate
     releaseFramebuffers <- newRefCounted (traverse_ release framebufferKeys)
     return (releaseFramebuffers, frameBuffers)
-
--- app/Render.hs
+++ app/Render.hs
@@ -1,1 +1,75 @@
+{-# LANGUAGE NumDecimals #-}
+
 module Render where
+
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
+import qualified Data.Vector as V
+import Data.Word
+import Frame
+import Init
+import Swapchain
+import Vulkan.Core10
+import qualified Vulkan.Core10.CommandBuffer as CommandBufferBeginInfo (CommandBufferBeginInfo (..))
+import qualified Vulkan.Core10.CommandBufferBuilding as RenderPassBeginInfo (RenderPassBeginInfo (..))
+import qualified Vulkan.Core10.FundamentalTypes as Extent2D (Extent2D (..))
+import Vulkan.Zero
+
+renderFrame :: (MonadResource m) => DeviceParams -> Frame -> m ()
+renderFrame DeviceParams{..} f@Frame{..} = do
+    let RecycledResources{..} = fRecycledResources
+        oneSecond = 1.0e9 :: Word64
+        SwapchainResources{..} = fSwapchainResources
+        SwapchainInfo{..} = srInfo
+
+    -- TODO: set up semaphores
+    imageIndex <- undefined
+
+    let commandBufferAllocateInfo =
+            zero
+                { commandPool = fCommandPool
+                , level = COMMAND_BUFFER_LEVEL_PRIMARY
+                , commandBufferCount = 1
+                }
+    (_, ~[commandBuffer]) <- withCommandBuffers dpDevice commandBufferAllocateInfo allocate
+
+    let commandBufferBeginInfo =
+            zero
+                { CommandBufferBeginInfo.flags = zero
+                , inheritanceInfo = Nothing
+                }
+
+    useCommandBuffer commandBuffer commandBufferBeginInfo $
+        myRecordCommandBuffer commandBuffer f imageIndex
+
+    undefined
+
+myRecordCommandBuffer :: (MonadIO m) => CommandBuffer -> Frame -> Word32 -> m ()
+myRecordCommandBuffer commandBuffer Frame{..} imageIndex = do
+    let SwapchainResources{..} = fSwapchainResources
+        SwapchainInfo{..} = srInfo
+        renderPassBeginInfo =
+            zero
+                { RenderPassBeginInfo.renderPass = fRenderPass
+                , RenderPassBeginInfo.framebuffer = fFramebuffers V.! fromIntegral imageIndex
+                , renderArea = Rect2D{offset = zero, extent = siImageExtent}
+                , clearValues = [Color (Float32 0.0 0.0 0.0 1.0)]
+                }
+    cmdUseRenderPass commandBuffer renderPassBeginInfo SUBPASS_CONTENTS_INLINE $ do
+        cmdSetViewport
+            commandBuffer
+            0
+            [ Viewport
+                { x = 0.0
+                , y = 0.0
+                , width = realToFrac $ Extent2D.width siImageExtent
+                , height = realToFrac $ Extent2D.height siImageExtent
+                , minDepth = 0.0
+                , maxDepth = 1.0
+                }
+            ]
+        cmdSetScissor
+            commandBuffer
+            0
+            [Rect2D{offset = Offset2D 0 0, extent = siImageExtent}]
+        cmdDraw commandBuffer 3 1 0 0
